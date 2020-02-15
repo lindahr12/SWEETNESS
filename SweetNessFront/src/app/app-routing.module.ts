8d7db5c8fc9aa6be77414b98b6f937ce2aa5f7ac
SahbiRomdhani <sahbiromdhani666@gmail.com>
2020-02-16 00:05:37 +0100

    fix navbar , footer et acceuil

--- SweetNessFront/src/app/app-routing.module.ts
+++ SweetNessFront/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [  
-  { path: 'accueil', component: AcceuilComponent },
+  { path: '', component: AcceuilComponent },
 ];
 
 @NgModule({
