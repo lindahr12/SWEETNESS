9bc2e259e8a39797fedbff8ebdf40ee9e7cfffab
linda hrichi <hrichilinda@yahoo.fr>
2020-03-04 17:01:24 +0100

    update

--- AdminSweetNess/src/app/app.module.ts
+++ AdminSweetNess/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BaseLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
