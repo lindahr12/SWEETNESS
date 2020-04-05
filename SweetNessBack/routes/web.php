048774a844fade4cd371ca0f1c9291901e8790c1
SahbiRomdhani <sahbiromdhani666@gmail.com>
2020-04-05 22:07:32 +0100

    laravel 7 and Jwt Config

--- SweetNessBack/routes/web.php
+++ SweetNessBack/routes/web.php
@@ -20,5 +20,18 @@ Route::group(['middleware' => ['cors']], function () {
         
     Route::get('/get-data','CategorieController@index');
     Route::post('/post-data','CategorieController@add');
+    Route::group([
+
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    
+    ], function ($router) {
+    
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    
+    });
     
 });
