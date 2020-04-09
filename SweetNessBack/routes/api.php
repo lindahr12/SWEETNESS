e74ca2b7e8b8114a0e9194538e3a2dc61d416f1c
linda hrichi <hrichilinda@yahoo.fr>
2020-04-10 00:15:50 +0200

    login angular

--- SweetNessBack/routes/api.php
+++ SweetNessBack/routes/api.php
@@ -1,21 +1,20 @@
 <?php
 
 Route::resource('/article','ArticleController');
-        
+
 Route::get('/get-data','CategorieController@index');
 Route::post('/post-data','CategorieController@add');
 Route::group([
 
     'middleware' => 'api',
     'prefix' => 'auth'
-    
+
 ], function ($router) {
-    
-    Route::post('login', 'AuthController@login');
+  Route::post('/login', 'AuthController@login');
     Route::post('/signup', 'AuthController@signup');
 
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
-    
+
 });
