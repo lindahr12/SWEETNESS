37e08ea7ac38844ac3ccf8fc52d8799747340b30
SahbiRomdhani <sahbiromdhani666@gmail.com>
2020-07-03 15:51:41 +0100

    fournisseur

---
+++ AdminSweetNess/src/app/home/fournisseur/fournisseur.component.ts
@@ -0,0 +1,132 @@
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import Swal from "sweetalert2";
+
+
+@Component({
+  selector: 'app-fournisseur',
+  templateUrl: './fournisseur.component.html',
+  styleUrls: ['./fournisseur.component.css']
+})
+export class FournisseurComponent implements OnInit {
+  private formfour: FormGroup;
+  private formupdate: FormGroup;
+
+  
+  fournisseur:any;
+  fournisseurupdated:any;
+
+  constructor(private http: HttpClient,private formBuilder: FormBuilder) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+  };
+  ngOnInit() {
+    /**Form Group store */
+    this.formfour =this.formBuilder.group({
+      nom: ['', Validators.required],
+      description: ['', Validators.required],
+      rue: ['', Validators.required],
+      region: ['', Validators.required],
+      num_tel: ['', Validators.required],
+      num_fax: ['', Validators.required],
+      email: ['', Validators.required],
+      matricule_fiscale: ['', Validators.required],
+      is_active: ['', Validators.required],
+    });
+    /**Form group update */
+    this.formupdate =this.formBuilder.group({
+      nom: ['', Validators.required],
+      description: ['', Validators.required],
+      rue: ['', Validators.required],
+      region: ['', Validators.required],
+      num_tel: ['', Validators.required],
+      num_fax: ['', Validators.required],
+      email: ['', Validators.required],
+      matricule_fiscale: ['', Validators.required],
+      is_active: ['', Validators.required],
+    });
+/**get all fournisseur */
+    this.http.get('http://127.0.0.1:8000/api/fournisseur').subscribe(data => {
+
+      console.log("fournisseurs is coming.",this.fournisseur = data);
+
+    }, error => console.error(error))
+
+  }
+  /**submitt */
+  submit() {
+    console.log(this.formfour.value);
+   this.http.post('http://127.0.0.1:8000/api/fournisseur', this.formfour.value
+  ).subscribe(data => {
+     console.log("four ajoutée");
+
+   });
+ }
+ /**delete */
+ delete(id: any) {
+
+  console.log(id);
+  Swal.fire({
+    title: 'Êtes-vous sûr?',
+    text: 'Vous ne pourrez pas récupérer!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Oui, supprimez-le!',
+    cancelButtonText: 'Non, garde-le'
+  }).then((result) => {
+    if (result.value) {
+      return this.http.delete('http://127.0.0.1:8000/api/fournisseur/' + id, this.httpOptions).subscribe(data => {
+          console.log("sucess");
+          Swal.fire(
+            'Deleted!',
+            'produit a été supprimé.',
+            'success'
+          )
+            window.location.reload();
+          //this.router.navigate["/home"];
+
+
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+    // For more information about handling dismissals please visit
+    // https://sweetalert2.github.io/#handling-dismissals
+    else if (result.dismiss === Swal.DismissReason.cancel) {
+      Swal.fire(
+        'Cancelled',
+        'Votre produit est sécurisé :)',
+        'error'
+      )
+    }
+  })
+
+}
+
+recuperer(id: any) {
+
+  this.http.get('http://127.0.0.1:8000/api/fournisseur/'+id).subscribe(data => {
+
+    console.log("fournisseur recuperer.",this.fournisseurupdated = data);
+
+  }, error => console.error(error))
+  console.log(this.fournisseurupdated);
+}
+/**submitt */
+update() {
+  console.log(this.formupdate.value);
+ this.http.put('http://127.0.0.1:8000/api/fournisseur/'+this.fournisseurupdated.id, this.formupdate.value
+).subscribe(data => {
+   console.log("four ajoutée");
+
+ });
+}
+
+}
